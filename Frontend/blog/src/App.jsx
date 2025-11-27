5349a199b7ef2dc2b3fec8404fe66a2a8a7dc9ae
M.Suleman <muhammadsulemanm431@gmail.com>
2025-11-27 15:26:31 +0500

    Update project

---
+++ Frontend/blog/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-3xl text-red-600 font-bold underline">
+        hello world
+      </div>
+    
+    </>
+  )
+}
+
+export default App
